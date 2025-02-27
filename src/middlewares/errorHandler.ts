7f978d922c64e72bed8da97dd9faf7df51cdc2ba
nahum casco <nahumcasco.v@gmail.com>
2025-02-27 18:35:40 -0500

    middleware fix return

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,16 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import AppError from "../utils/appError";
 
-const errorHandler = (error: any, req: Request, res: any, next: NextFunction) => {
+const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
     console.log(error);
 
     if (error instanceof AppError) {
-        return res.status(error.statusCode).json({
+        res.status(error.statusCode).json({
             error: error.message,
         });
+        return;
     }
 
-    return res.status(500).send("Internal Server Error");
+    res.status(500).send("Internal Server Error");
+    return;
 };
 
 // module.exports = errorHandler;
--- src/modules/auth/authMiddleware.ts
+++ src/modules/auth/authMiddleware.ts
@@ -1,22 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 
-const authMiddleware = (req: Request, res: any, next: NextFunction) => {
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers['authorization']?.split(' ')[1]; // get token from header 'Authorization'
 
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized access, token not provided' });
+    res.status(401).json({ message: 'Unauthorized access, token not provided' });
+    return;
   }
 
   const jwtSecret = process.env.JWT_SECRET;
 
   if (!jwtSecret) {
-    return res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: 'Internal Server Error' });
+    return;
   }
 
   jwt.verify(token, jwtSecret, (err: any, decoded: any) => {
     if (err) {
-      return res.status(403).json({ message: 'Invalid token' });
+      res.status(403).json({ message: 'Invalid token' });
+      return; 
     }
     (req as any).user = decoded;
     next();
